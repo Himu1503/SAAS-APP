62e645743bd5a4c17eaba9393938c93cb247e495
Himanshu Saxena <himanshu.saxena.india@gmail.com>
2024-07-03 22:26:02 +0100

    python decouple

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+from decouple import config
 
 from pathlib import Path
 
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g9_yev3@ooi*u*xjxn#_2=1tp(&wiws*!u&=$ufn3-mf)r0mvg'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = str(os.environ.get("DJANGO_DEBUG").lower() == "true")
+DEBUG = config("DJANGO_DEBUG",cast = bool)
 
 ALLOWED_HOSTS = [
      ".railway.app"
